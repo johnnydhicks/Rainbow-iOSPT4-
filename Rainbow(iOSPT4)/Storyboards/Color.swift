6ef05ce9c74e4fc7cbc984b767597d7c88656685
Johnny Hicks <johnnydhicks@gmail.com>
2019-11-12 20:31:41 -0700

    Set up initial views

---
+++ Rainbow(iOSPT4)/Storyboards/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Rainbow(iOSPT4)
+//
+//  Created by Johnny Hicks on 11/12/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+struct MyColor {
+    let name: String
+    let color: UIColor
+}
